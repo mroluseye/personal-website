f1bc95e7363be1f619f9ad52a9fbe613cb93cdd5
mroluseye <45568545+mroluseye@users.noreply.github.com>
2025-05-10 22:05:14 +0100

    Update HeroSection.tsx
    Picture Update

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -225,7 +225,7 @@ const HeroSection = ({
             className="w-48 h-48 rounded-full overflow-hidden border-4 border-white shadow-lg"
           >
             <img
-              src="https://media.licdn.com/dms/image/v2/D4E03AQEa3kLXxobxkQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1673570225604?e=1751500800&v=beta&t=s16nmnlUDu02g2O5zpxqfJOvXetUcBM1dyNBNzA8vAQ"
+              src="https://scontent-lhr6-1.xx.fbcdn.net/v/t51.75761-15/497281826_18507622468037482_554299333579073425_n.jpg?stp=dst-jpegr_tt6&_nc_cat=110&ccb=1-7&_nc_sid=127cfc&_nc_ohc=vIos_gD9R6sQ7kNvwEFgYqp&_nc_oc=AdkFC90-ueGuYEmn_fPBEJwHIbG39VJ3aHiTexLV-0kSiR3A6T25_tQeOlPhdV6K4uQ&_nc_zt=23&se=-1&_nc_ht=scontent-lhr6-1.xx&_nc_gid=eE5tkDkf-gDa53d-4b2LNQ&oh=00_AfIhofFt829EyyljrLr7SLWy2_Jw0GUFrD1yRy6ZU3xodw&oe=68257B8E"
               alt="Profile"
               className="w-full h-full object-cover"
             />
